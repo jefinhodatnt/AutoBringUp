a56df8f5c9016e35874fe2e27870b72b3d496ef8
broly <jpegrande@gmail.com>
2021-06-14 10:54:54 -0400

    ignore-missing-.dependecy-file

--- autobringup.sh
+++ autobringup.sh
@@ -10,28 +10,27 @@ cat << "EOF"
 EOF
 printf "\n\n"
 printf "WARNING MAKE SURE TO ENTER THE CORRECT ROM NAME\n OR IT WILL RENAME SOME FILES WRONGLY!!!\n\n"
-read -p "Please enter the name of the old rom: " oldrom
-read -p "Now please enter the name of the new rom: " newrom
+read -r -p "Please enter the name of the old rom: " oldrom
+read -r -p "Now please enter the name of the new rom: " newrom
 printf "\n\n"
 
 if [[ -d "device/motorola/ali" ]] 
 then
-	cd device/motorola/ali
+	cd device/motorola/ali || exit
 else
 	printf "path to device doesn't exist!!\n"
 	exit 1
 fi
 
 if [[ -f "$oldrom.dependencies" ]]; then
-	mv "$oldrom".dependencies "$newrom".dependencies
+	mv "$oldrom".dependencies "$newrom".dependencies || :
 else 
-	printf "There's no file named "$oldrom.dependencies"!!\n"
-	exit 1
+	printf "There's no file .dependencies in /device/motorola/ali ignoring\n!!"
 fi
 
 if [[ -f "$oldrom"_ali.mk ]]; then
 	mv "$oldrom"_ali.mk "$newrom"_ali.mk
-	sed -i 's/'$oldrom'/'$newrom'/g' *.*
+	sed -i 's/'"$oldrom"'/'"$newrom"'/g' ./*.*
 	cd ../../../
 else
 	printf "file not found please make sure your device tree is correct!\n"
@@ -40,18 +39,18 @@ fi
 
 if [[ -d "device/motorola/msm8953-common" ]]
 then 
-	cd device/motorola/msm8953-common
+	cd device/motorola/msm8953-common || exit
 else 
 	printf "path to device doesn't exist"
 	exit1
 fi
 
 if [[ -f "$oldrom.dependencies" ]]; then
-	mv "$oldrom".dependencies "$newrom".dependencies
-	sed -i 's/'$oldrom'/'$newrom'/g' *.*
+	mv "$oldrom".dependencies "$newrom".dependencies || :
+	sed -i 's/'"$oldrom"'/'"$newrom"'/g' ./*.*
 	cd ../../../
 else
-	printf "file not found please make sure your device tree is correct!\n"
+	printf "There's no file .dependencies in /device/motorola/msm8953-common ignoring\n!!"
 fi
 
 printf "\n\n"
